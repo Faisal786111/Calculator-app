635d007454c0318901c4f4dbeb9582129eb1d405
Faisal786111 <faisalkhanisrar@gmail.com>
2024-02-28 17:50:58 +0530

    Keydown Listner Added

--- index.js
+++ index.js
@@ -1,30 +1,95 @@
 var screen = document.getElementsByName('screen')[0];
-var del = document.querySelector(".del");
-var equal = document.querySelector(".equal");
+var firstContianer = document.querySelector(".first-container");
 var toggle = document.querySelector(".toggle");
+var equal = document.querySelector(".equal");
+var del = document.querySelector(".del");
 var one = document.querySelector(".one");
 var two = document.querySelector(".two");
 var three = document.querySelector(".three");
-var firstContianer = document.querySelector(".first-container");
+var button1 = document.querySelector(".button-1");
 
+document.addEventListener("keydown" , function({key}) {
+    switch(key){
+        case "1" : 
+            screen.value += key;
+        break;
 
+        case "2" : 
+            screen.value += key;
+        break;
+        case "3" :
+            screen.value += key;
+        break;
+        case "4" :
+            screen.value +=key;
+        break;
+        case "5" :
+            screen.value +=key;
+        break;
+        case "6" :
+            screen.value +=key;
+        break;
+        case "7" :
+            screen.value +=key;
+        break;
+        case "8" :
+            screen.value +=key;
+        break;
+        case "9" :
+            screen.value +=key;
+        break;
+        case "0" :
+            screen.value +=key;
+        break;
+        case "Delete" :
+            deleteLetter();
+        break;
 
-equal.addEventListener("click" , function () {
-    if(screen.value.length > 1){
-        screen.value = eval(screen.value).toString();
+        case "=" :
+            evaluate();
+        break;
+
+        case "+" :
+            screen.value +=key;
+        break;
+        case "-" :
+            screen.value +=key;
+        break;
+        case "/" :
+            screen.value +=key;
+        break;
+        case "*" :
+            screen.value +=key;
+        break;
+        case "." :
+            screen.value +=key;
+        break;
+        case "Escape" :
+            screen.value ="";
+        break;
     }
 })
 
+equal.addEventListener("click" , evaluate)
+
+function evaluate() {
+    if(screen.value.length > 1){
+        screen.value = eval(screen.value).toString();
+    }
+}
 
 
-del.addEventListener("click" , function () {
+
+del.addEventListener("click" , deleteLetter)
+
+function deleteLetter() {
     if(screen.value.length  === 1){
         screen.value = "";
     }else {
         screen.value = screen.value.substring(0 , screen.value.length-1).toString();
         console.log(typeof screen.value.substring(0 , screen.value.length-1));
     }
-})
+}
 
 one.addEventListener("click" , function(){
     toggle.style.left = "7px"
@@ -48,6 +113,3 @@ three.addEventListener("click" , function(){
     document.body.classList.add("theme3");
 
 })
-
-
-
